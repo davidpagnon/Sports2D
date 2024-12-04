cab0282f0e9672a411065c7bf179ab8da31163b9
davidpagnon <contact@david-pagnon.com>
2024-12-04 22:53:17 +0100

    weird test was here for a reason

--- Sports2D/Utilities/tests.py
+++ Sports2D/Utilities/tests.py
@@ -35,7 +35,7 @@ def test_workflow():
     Test the workflow of Sports2D.
     '''
 
-    # From python
+    ## From python
     config_path = Path.cwd().parent / 'Demo' / 'Config_demo.toml'
     config_dict = toml.load(config_path)
     config_dict.get("project").update({"video_dir":'../Demo'})
@@ -46,19 +46,25 @@ def test_workflow():
     Sports2D.process(config_dict)
 
 
-    # From command line (CLI)
+    ## From command line (CLI)
     # Default
     demo_cmd = ["sports2d", "--show_realtime_results", "False", "--show_graphs", "False"]
     subprocess.run(demo_cmd, check=True, capture_output=True, text=True)
 
-    # From config file
-    demo_cmd2 = ["sports2d", "--config", str(config_path), "--show_realtime_results", "False", "--show_graphs", "False"]
-    subprocess.run(demo_cmd2, check=True, capture_output=True, text=True)
-
     # With custom parameters
-    demo_cmd3 = ["sports2d", "--to_meters", "False", "--multiperson", "False", "--mode", "lightweight", "--det_frequency", "50", "--time_range", "1.2 2.7",  "--slowmo_factor", "4", "--show_realtime_results", "False", "--show_graphs", "False"]
-    subprocess.run(demo_cmd3, check=True, capture_output=True, text=True)
+    demo_cmd2 = ["sports2d", "--to_meters", "False", "--multiperson", "False", "--mode", "lightweight", "--det_frequency", "50", "--time_range", "1.2 2.7",  "--slowmo_factor", "4", "--show_realtime_results", "False", "--show_graphs", "False"]
+    subprocess.run(demo_cmd2, check=True, capture_output=True, text=True)
     
     # With inverse kinematics
-    # demo_cmd4 = ["sports2d", "--do_ik", "--person_orientation", "front none left", "--show_realtime_results", "False", "--show_graphs", "False"]
-    # subprocess.run(demo_cmd4, check=True, capture_output=True, text=True)
+    # demo_cmd3 = ["sports2d", "--do_ik", "--person_orientation", "front none left", "--show_realtime_results", "False", "--show_graphs", "False"]
+    # subprocess.run(demo_cmd3, check=True, capture_output=True, text=True)
+    
+    # From config file
+    cli_config_path = Path(__file__).resolve().parent.parent / 'Demo' / 'Config_demo.toml'
+    config_dict = toml.load(cli_config_path)
+    cli_video_dir = Path(__file__).resolve().parent.parent / 'Demo'
+    config_dict.get("project").update({"video_dir": str(cli_video_dir)})
+    with open(cli_config_path, 'w') as f: toml.dump(config_dict, f)
+
+    demo_cmd4 = ["sports2d", "--config", str(cli_config_path), "--show_realtime_results", "False", "--show_graphs", "False"]
+    subprocess.run(demo_cmd4, check=True, capture_output=True, text=True)
