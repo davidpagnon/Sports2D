1623c6af2a7b9f8046a259e0ab6adedd8a9c68fc
David PAGNON <contact@david-pagnon.com>
2026-05-03 11:37:07 +0200

    fixed last commit

--- Sports2D/Utilities/tests.py
+++ Sports2D/Utilities/tests.py
@@ -100,7 +100,8 @@ def test_workflow():
                  "--nb_persons_to_detect", "1", "--person_ordering_method", "greatest_displacement", 
                  "--mode", "lightweight", "--det_frequency", "50", 
                  "--slowmo_factor", "4",
-                 "--filter_type", "gaussian", "--use_augmentation", "False",
+                 "--filter_type", "acc_minimizing", "----accminimizing_cut_off_frequency", "4",
+                 "--use_augmentation", "False",
                  "--pose_model", "body", "--mode", """{'pose_class':'RTMO', 'pose_model':'https://download.openmmlab.com/mmpose/v1/projects/rtmo/onnx_sdk/rtmo-m_16xb16-600e_body7-640x640-39e78cc4_20231211.zip', 'pose_input_size':[640, 640]}"""]
     subprocess.run(demo_cmd3, check=True, capture_output=True, text=True, encoding='utf-8', errors='replace')
     
